a881b356c1f55e9aa31ffb238a0812ea58c3566d
Zane Chumley <zane.chumley@my.utsa.edu>
2024-06-07 12:28:57 -0500

    Build 1.0.0
    Inaugural launch

---
+++ RunningAssistance.R
@@ -0,0 +1,10 @@
+# Zane Chumley
+# DA-6833-02T-202430
+# Summer 2024
+# School of Data Science
+# University of Texas at San Antonio
+#
+# Go Roadrunners!
+#
+# Install packages as needed
+install.packages("skimr")
